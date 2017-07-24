f629a4e661e541398bd5474b73f9f2ff9c6599ea
amitabh.rai <amitabh.rai@sephora.com>
2017-07-24 15:21:43 +0530

    change

--- src/test/java/Step_definition/YaahooRunnerTest.java
+++ src/test/java/Step_definition/YaahooRunnerTest.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/Step_definition/logIn.feature" ,
+@CucumberOptions(features="src/test/java/Step_definition/logIn.feature" , glue={"Step_definition"},
 			plugin={"html:target/cucumber-html-report"})
 
 public class YaahooRunnerTest {
